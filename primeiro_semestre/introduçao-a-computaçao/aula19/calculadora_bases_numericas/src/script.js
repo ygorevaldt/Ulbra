01876da7381a5ea8c77c26f20d443d4c86c4ff16
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-06-27 19:34:35 -0300

    finalização calculadora de bases numéricas

--- primeiro_semestre/introduçao-a-computaçao/aula19/calculadora_bases_numericas/src/script.js
+++ primeiro_semestre/introduçao-a-computaçao/aula19/calculadora_bases_numericas/src/script.js
@@ -3,8 +3,17 @@ function converteNumero(){
   let baseFinal = parseInt(document.querySelector("#baseFinal").value);
   let numero = document.querySelector("#numero").value;
   let resultadoDaConversao = document.querySelector("#resultado");
+
+  // verificando o input dos dados
+  if(baseInicial === baseFinal) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong> <span class="text-primary">Não há conversão a ser feita</span>`;
+  }
   
-  // Qualquer base pra decimal
+  // Binario pra decimal
   function binarioParaDecimal(numero) {
     numero = numero.toString()
     numero = numero.split("");
@@ -22,9 +31,45 @@ function converteNumero(){
       }
       aux++;
     }
-    return resultado;
+    return resultado / baseInicial;
+  }
+  if(baseInicial == 2 && baseFinal == 10) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${binarioParaDecimal(numero)}<span class="small">(${baseFinal})</span></span>`;
   }
 
+  // Octal pra decimal
+  function octalParaDecimal(numero) {
+    numero = numero.toString()
+    numero = numero.split("");
+    let potencia = 1;
+    let aux = 0;
+    let resultado = 0;
+    let limiteLaco = numero.length -1
+    for(let i = limiteLaco; i >= 0; i--) {
+      if (aux == 0) {
+        resultado += parseInt(numero[i]) * (baseInicial * 1);
+      }
+      if (aux > 0) {
+        potencia *= baseInicial;
+        resultado += parseInt(numero[i]) * (baseInicial * potencia);
+      }
+      aux++;
+    }
+    return resultado / baseInicial;
+  }
+  if(baseInicial == 8 && baseFinal == 10) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${octalParaDecimal(numero)}<span class="small">(${baseFinal})</span></span>`;
+  }
+
+  // Hexadecimal pra decimal
   function hexadecimalParaDecimal(numero) {
     numero = numero.toString()
     numero = numero.split("");
@@ -68,33 +113,17 @@ function converteNumero(){
       }
       aux++;
     }
-    return resultado;
+    return resultado / baseInicial;
   }
-  
-  if(baseInicial === baseFinal) {
+  if(baseInicial == 16 && baseFinal == 10) {
     if (numero.length === 0) {
       resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
       return;
     }
-    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong> <span class="text-primary">Não há conversão a ser feita</span>`;
-  }
-  else if(baseInicial == 2 || baseInicial == 8 && baseFinal == 10) {
-    if (numero.length === 0) {
-      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
-      return;
-    }
-    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${binarioParaDecimal(numero) / baseInicial}<span class="small">(${baseFinal})</span></span>`;
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${hexadecimalParaDecimal(numero)}<span class="small">(${baseFinal})</span></span>`;
   }
 
-  else if(baseInicial == 16 && baseFinal == 10) {
-    if (numero.length === 0) {
-      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
-      return;
-    }
-    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${hexadecimalParaDecimal(numero) / baseInicial}<span class="small">(${baseFinal})</span></span>`;
-  }
-
-  // decimal para binario, octal, hexadecimal
+  // função para usar na conversão de decimal para binario, octal, hexadecimal
   function inverteResultado(resultado) {
     resultado = resultado.split("");
     let resultadoFinal  = "";
@@ -103,6 +132,8 @@ function converteNumero(){
     }
     return resultadoFinal;
   }
+
+  //Decimal para hexadecimal 
   function decimalParaHexadecimal(numero){
     let valor = "";
     switch (numero) {
@@ -127,10 +158,10 @@ function converteNumero(){
       default:
         valor = numero;
     }
-
     return valor;
   }
   
+  // decimal para binario
   function decimalParaBinario(numero, baseFinal) {
     let dividendo =  numero;
     let resultado = "";
@@ -164,8 +195,90 @@ function converteNumero(){
     }
     resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${decimalParaBinario(numero, baseFinal)}<span class="small">(${baseFinal})</span></span>`;
   } 
+
+  // binario para octal
+  function binarioParaOctal(numero) {
+    let resultado = binarioParaDecimal(numero) / 2;
+    let resultadoFinal = decimalParaBinario(resultado, 8);
+    return resultadoFinal;
+  }
+  if (baseInicial == 2 && baseFinal == 8) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${binarioParaOctal(numero)}<span class="small">(${baseFinal})</span></span>`;
+  } 
+
+  // Binario para hexadecimal
+  function binarioParaHexadecimal(numero) {
+    let resultado = binarioParaDecimal(numero);
+    let resultadoFinal = decimalParaHexadecimal(resultado);
+    return resultadoFinal;
+  }
+  if (baseInicial == 2 && baseFinal == 16) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${binarioParaHexadecimal(numero)}<span class="small">(${baseFinal})</span></span>`;
+  } 
+
+  // Octal para binario
+  function octalParaBinario(numero) {
+    let resultado = octalParaDecimal(numero);
+    console.log(resultado);
+    let resultadoFinal = decimalParaBinario(resultado, baseFinal);
+    return resultadoFinal;
+  }
+  if (baseInicial == 8 && baseFinal == 2) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${octalParaBinario(numero)}<span class="small">(${baseFinal})</span></span>`;
+  } 
+
+  // Octal para hexadecimal
+  function octalParaHexadecimal(numero) {
+    let resultado = octalParaDecimal(numero);
+    console.log(resultado);
+    let resultadoFinal = decimalParaBinario(resultado, baseFinal);
+    return resultadoFinal;
+  }
+  if (baseInicial == 8 && baseFinal == 16) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${octalParaHexadecimal(numero)}<span class="small">(${baseFinal})</span></span>`;
+  }
+  
+  // Hexadeciamal pra binario
+  function hexadecimalParaBinario(numero) {
+    let resultado = hexadecimalParaDecimal(numero);
+    let resultadoFinal = decimalParaBinario(resultado, baseFinal);
+    return resultadoFinal;
+  }
+  if (baseInicial == 16 && baseFinal == 2) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${hexadecimalParaBinario(numero)}<span class="small">(${baseFinal})</span></span>`;
+  }
+
+  //  Hexadecimal para octal
+  function hexadecimalPraOctal(numero) {
+    let resultado = hexadecimalParaDecimal(numero);
+    let resultadoFinal = decimalParaBinario(resultado, baseFinal);
+    return resultadoFinal;
+  }
+  if (baseInicial == 16 && baseFinal == 8) {
+    if (numero.length === 0) {
+      resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class = "text-danger">ERRO! O número a ser convertido não foi encontrado.</span>`;
+      return;
+    }
+    resultadoDaConversao.innerHTML = `<strong>Resultado: </strong><span class="text-secondary">${hexadecimalPraOctal(numero)}<span class="small">(${baseFinal})</span></span>`;
+  }
 }
-
-
-
-

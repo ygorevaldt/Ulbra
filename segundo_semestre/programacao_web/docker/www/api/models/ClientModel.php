ff818801edf42291ebfb54a78f7dc72cfced36e1
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-10-23 18:28:52 -0300

    adicionado classes ClientModel e ClientController do diretorio API

--- segundo_semestre/programacao_web/docker/www/api/models/ClientModel.php
+++ segundo_semestre/programacao_web/docker/www/api/models/ClientModel.php
@@ -12,5 +12,43 @@ class ClientModel
 
   public function listClients()
   {
+    $sql = "SELECT * FROM clients";
+    return $this->connection->query($sql);
+  }
+
+  public function consultClient($id_client)
+  {
+    $sql = "SELECT * FROM clients WHERE id_cliente = $id_client";
+    return $this->connection->query($sql);
+  }
+
+  public function insertClient($arrayClient)
+  {
+    $sql = "INSERT INTO clients (name, email, phone, address) VALUES (
+      '{$arrayClient['name']}',
+      '{$arrayClient['email']}',
+      '{$arrayClient['phone']}',
+      '{$arrayClient['address']}'
+      )";
+    return $this->connection->query($sql);
+  }
+
+  public function updateClient($id_client, $arrayClient)
+  {
+    $sql = "UPDATE clients SET
+      name = '{$arrayClient['name']}',
+      email = '{$arrayClient['email']}',
+      phone = '{$arrayClient['phone']}',
+      address = '{$arrayClient['address']}'
+    WHERE
+      id_cliente = {$id_client}
+    ";
+    return $this->connection->query($sql);
+  }
+
+  public function deleteClient($id_client)
+  {
+    $sql = "DELETE FROM clients WHERE id_client = {$id_client}";
+    return $this->connection->query($sql);
   }
 }

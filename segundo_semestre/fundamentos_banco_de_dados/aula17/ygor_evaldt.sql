4b88997a7c8822d42bcd824aa97c88efbe4dae3e
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-11-23 20:28:42 -0300

    commit

--- segundo_semestre/fundamentos_banco_de_dados/aula17/ygor_evaldt.sql
+++ segundo_semestre/fundamentos_banco_de_dados/aula17/ygor_evaldt.sql
@@ -79,12 +79,8 @@ INSERT INTO livros(titulo, preco, lancamento, id_assunto, id_editora) VALUES
     ('Banco de Dados Distribuídos', 31.20, '1999-01-10','B', 1),
     ('Banco de Dados Distribuído', 31.20, '1999-01-10','B', 1);
 
+
 -- 3 – Crie comandos SQLs que retorne os dados solicitados abaixo.
-
--- a) Monte um comando para excluir da tabela livros aqueles que possuem o código maior ou igual a 2, que possuem o preço maior que R$ 50,00 e que já foram lançados.
-DELETE FROM livros WHERE id >= 2 AND preco > 50 AND lancamento < '2022-11-19';
-
--- b) Escreva o comando que seleciona as colunas NOME, CPF e ENDERECO, da tabela AUTOR, para aqueles que possuem a palavra ‘joão’ no nome.
 INSERT INTO autores(nome, cpf, data_nascimento, nacionalidade) VALUES 
     ('João Cassio Costa', '12345678910', '1983-05-18', 'Brasileiro'),
     ('Alessandra Silveira', '04435480050', '2003-03-22', 'Brasileiro'),
@@ -92,34 +88,49 @@ INSERT INTO autores(nome, cpf, data_nascimento, nacionalidade) VALUES
     ('Ramon Lumertz', '10987654321', '1981-12-12', 'Brasileiro'),
     ('Maria Elena Schutt Da Silveira', '15975345610', '1963-04-09', 'Brasileiro');
 
+INSERT INTO autores(nome, cpf, data_nascimento, endereco, nacionalidade) VALUES 
+    ('Carioca da Gema', '12345678920', '2022-11-19', 'Rio De Janeiro', 'Brasileiro');
+
+-- a) Monte um comando para excluir da tabela livros aqueles que possuem o código maior ou igual a 2, que possuem o preço maior que R$ 50,00 e que já foram lançados.
+DELETE FROM livros WHERE id >= 2 AND preco > 50 AND lancamento < CURRENT_DATE;
+
+-- b) Escreva o comando que seleciona as colunas NOME, CPF e ENDERECO, da tabela AUTOR, para aqueles que possuem a palavra ‘joão’ no nome.
 SELECT nome, cpf, endereco FROM autores WHERE nome LIKE '%João%';
 
 -- c) Excluir o livro cujo título é ‘BANCO DE DADOS DISTRIBUÍDO’ ou ‘BANCOS DE DADOS DISTRIBUÍDOS’. Somente essas duas opções devem ser consideradas.
-DELETE FROM livros WHERE titulo LIKE 'Banco de Dados Distribuído%';
+DELETE FROM livros WHERE titulo LIKE 'Banco_ de Dado_ Distribuído_';
 
 -- d) Selecione o nome e o CPF de todos os autores que nasceram após 01 de janeiro de 1990.
 SELECT nome, cpf FROM autores WHERE data_nascimento > '1990-01-01';
 
 -- e) Selecione a matrícula e o nome dos autores que possuem RIO DE JANEIRO no seu endereço.
-INSERT INTO autores(nome, cpf, data_nascimento, endereco, nacionalidade) VALUES 
-    ('Carioca da Gema', '12345678920', '2022-11-19', 'Rio De Janeiro', 'Brasileiro');
-
 SELECT matricula, nome FROM autores WHERE endereco LIKE '%Rio De Janeiro%';
 
 -- f) Atualize para zero o preço de todos os livros onde a data de lançamento for nula ou onde seu preço atual for inferior a R$ 55,00.
 UPDATE livros SET preco = 0 WHERE lancamento IS NULL OR preco < 55;
 
 -- g) Exclua todos os livros onde o assunto for diferente de ‘S’, ‘P’, ou ‘B’.
-DELETE FROM livros WHERE id_assunto != 'S' AND id_assunto != 'P' AND id_assunto != 'B';
+DELETE FROM livros WHERE id_assunto NOT IN('S', 'P', 'B');
 
 -- h) Escreva o comando para contar quantos são os autores cadastrados na tabela AUTORES.
 SELECT COUNT(matricula) AS total_de_autores_cadastrados FROM autores;
 
 -- i) Escreva o comando que apresenta qual o número médio de autores de cada livro. Você deve utilizar, novamente, a tabela AUTOR_LIVRO. 
+SELECT avg(quantos) FROM (
+    SELECT id_livro, COUNT(matricula_autores) AS quantos
+    FROM autores_livros    
+    GROUP BY id_livro
+) AS minha_tabela;
 
 -- j) Apresente o comando SQL para gerar uma listagem dos códigos dos livros que possuem a menos dois autores.
+SELECT id_livro, COUNT(matricula_autores) AS quantos
+FROM autores_livros
+GROUP BY id_livro
+HAVING quantos >= 2;
+
 
 -- k) Escreva o comando para apresentar o preço médio dos livros por código de editora. Considere somente aqueles que custam mais de R$ 45,00.
+
 
 -- l) Apresente o preço máximo, o preço mínimo e o preço médio dos livros cujos assuntos são ‘S’ ou ‘P’ ou ‘B’, para cada código de editora.
 

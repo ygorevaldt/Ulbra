8135468a02613256b017a9d71e7a3ddd7883060a
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2022-11-24 19:44:50 -0300

    exercicio tde aula 17

---
+++ segundo_semestre/engenharia_software/aula17/tde.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+# passo 1
+driver = webdriver.Chrome()
+driver.get('https://www.google.com')
+
+# passo 2
+import time
+browser = webdriver.Chrome()
+browser.get("https://www.browserstack.com/users/sign_in") 
+time.sleep(10)
+user = browser.find_element_by_id("user_email_login")
+password = browser.find_element_by_id("user_password")
+user.send_keys("evaldtygor@gmail.com")
+password.send_keys("codeislife")
+login_attempt = browser.find_element_by_xpath("//*[@type='submit']")
+login_attempt.submit()
+browser.quit()
+
+

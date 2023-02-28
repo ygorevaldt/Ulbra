7748b748d3bdbb5babc25910bc0f64437dfba7d6
Ygor-Evaldt-dev <evaldtygor@rede.ulbra.br>
2023-02-27 21:10:54 -0300

    adicionado exercicio aula 01 lab.programacao

---
+++ terceiro_semestre/laboratorio-de-programacao/aula01_hello_word/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void printMessage(String message) {
+        System.out.println(message);
+    }
+
+    public static void main(String[] args) {
+        printMessage("Hello world !!");
+    }
+}
